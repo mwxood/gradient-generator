8b8cd91c78367435ecf11cad81b5741ccd1235d6
Mihail <mwxood@gmail.com>
2019-04-15 19:26:20 +0300

    Fixed

--- scripts.js
+++ scripts.js
@@ -1,27 +1,46 @@
-var css = document.querySelector(".gradient-style"),
-    color1 = document.querySelector(".color1"),
+
+class Gradient {
+    constructor(css) {
+        this.css = document.querySelector(css);
+    }
+
+    getGradientValue(body) {
+        return this.css.textContent = body.style.background + ";";
+    }
+
+    setGradient(body, color1, color2) {
+        body.style.background =
+        "linear-gradient(to right," +  color1.value + "," + color2.value +")";
+        this.getGradientValue(body);
+    }
+
+    static getRandomColor() {
+        return "#" + Math.random().toString(16).slice(2,8);
+    }
+
+    setRandomColor(body) {
+        body.style.background =
+            "linear-gradient(to right," +  Gradient.getRandomColor() + "," + Gradient.getRandomColor(); +")";
+        this.getGradientValue(body);
+    }
+}
+
+
+
+let color1 = document.querySelector(".color1"),
     color2 = document.querySelector(".color2"),
     body = document.getElementById("gradient"),
     randomColorButton = document.querySelector(".random-color");
 
-function setGradient() {
-    body.style.background =
-        "linear-gradient(to right," +  color1.value + "," + color2.value +")";
+const setColor = new Gradient(".gradient-style");
+setColor.getGradientValue(body);
 
-    css.textContent = body.style.background + ";";
-}
-
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
-
-randomColorButton.addEventListener("click", function() {
-    body.style.background =
-        "linear-gradient(to right," +  randomColor() + "," + randomColor(); +")";
-    css.textContent = body.style.background + ";";
+color1.addEventListener("input", () => {
+    setColor.setGradient(body, color1, color2);
 });
-
-function randomColor() {
-    return "#" + Math.random().toString(16).slice(2,8).toLocaleUpperCase();
-}
-css.textContent = body.style.background + ";";
-
+color2.addEventListener("input", () => {
+    setColor.setGradient(body, color1, color2);
+});
+randomColorButton.addEventListener("click", () => {
+    setColor.setRandomColor(body);
+});
